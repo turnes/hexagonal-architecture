acf5cf0f0957584e420cab3597ac22979ed4cd0e
turnes <rafael@turnes.com.br>
2021-06-22 19:16:13 -0300

    feature product_service

---
+++ app/product_service.go
@@ -0,0 +1,52 @@
+package app
+
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (s *ProductService) Get(id string) (ProductInterface, error) {
+	product, err := s.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+	_, err := product.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.Persistence.Save(product)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *ProductService) Enable(product ProductInterface) (ProductInterface, error){
+	err := product.Enable()
+	if err != nil {
+		return nil, err
+	}
+	_, err = s.Persistence.Save(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (s *ProductService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+	if err != nil {
+		return nil, err
+	}
+	_, err = s.Persistence.Save(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
